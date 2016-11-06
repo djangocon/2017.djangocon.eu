d3e4a30257cac5e636375b0449c9b9ecd01fb5e8
Iacopo Spalletti <i.spalletti@nephila.it>
2016-11-06 18:21:51 +0100

    Code style fixes

--- conference/cfp/models.py
+++ conference/cfp/models.py
@@ -2,8 +2,8 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
 from django.db import models
-from django.utils.translation import gettext_lazy as _
 from django.utils.encoding import python_2_unicode_compatible
+from django.utils.translation import gettext_lazy as _
 
 
 @python_2_unicode_compatible
--- conference/cfp/workshop_urls.py
+++ conference/cfp/workshop_urls.py
@@ -3,12 +3,12 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from django.conf.urls import url
 
-from .views import SubmissionView
 from .forms import WorkshopSubmissionForm
 from .models import WorkshopSubmission
+from .views import SubmissionView
 
 urlpatterns = [
-    url(r'^', SubmissionView.as_view(form_class=WorkshopSubmissionForm,
-                                     model=WorkshopSubmission),
-                                     name='cfp_workshop_submission'),
+    url(r'^', SubmissionView.as_view(
+        form_class=WorkshopSubmissionForm, model=WorkshopSubmission
+    ), name='cfp_talks_submission'),
 ]
