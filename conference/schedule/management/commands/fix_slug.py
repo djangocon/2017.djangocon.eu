31716e64f91a234f3a1397d5559b66a1600c822a
Iacopo Spalletti <i.spalletti@nephila.it>
2017-01-31 21:21:39 +0100

    Schedule / talk detail pages

---
+++ conference/schedule/management/commands/fix_slug.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
+
+from django.core.management.base import NoArgsCommand
+
+from conference.schedule.models import Slot
+
+
+class Command(NoArgsCommand):
+
+    def handle_noargs(self, **options):
+        for slot in Slot.objects.all():
+            slot.save()
