33bc22d7e98d02ed834de8c84f517af06c41c5b1
Iacopo Spalletti <i.spalletti@nephila.it>
2017-02-13 12:29:09 +0100

    Fix grid

--- djangocon_europe/management/commands/fix_grid.py
+++ djangocon_europe/management/commands/fix_grid.py
@@ -10,14 +10,14 @@ from conference.schedule.models import Slot
 class Command(NoArgsCommand):
 
     def handle_noargs(self, **options):
-        sections = [4577, 4554, 4564, 4550, 4558, 4557, 4548, 5016, 5007, 5016, 4999, 5003, 5014]
+        sections = [5001, 5020, 5011, 5003, 5007, 5018]
         plugins = CMSPlugin.objects.filter(parent__in=sections)
         levels = {
-            4577: 'sponsors-row__platinum',
-            4558: 'sponsors-row__gold',
-            4550: 'sponsors-row__bronze',
-            4554: 'sponsors-row__special',
-            4564: 'sponsors-row__media',
+            5020: 'sponsors-row__platinum',
+            5011: 'sponsors-row__gold',
+            5003: 'sponsors-row__bronze',
+            5007: 'sponsors-row__special',
+            5018: 'sponsors-row__media',
         }
         for plugin in plugins:
             plugin, __ = plugin.get_plugin_instance()
