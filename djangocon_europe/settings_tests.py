0cae1ae6ca372df40864feb2c45763bc2640890b
Iacopo Spalletti <i.spalletti@nephila.it>
2017-03-07 20:49:23 +0100

    Disable migrations

--- djangocon_europe/settings_tests.py
+++ djangocon_europe/settings_tests.py
@@ -15,3 +15,14 @@ DATABASES = {
 }
 
 RAVEN_CONFIG = {}
+
+
+class DisableMigrations(object):
+    def __contains__(self, item):
+        return True
+
+    def __getitem__(self, item):
+        return 'notmigrations'
+
+
+MIGRATION_MODULES = DisableMigrations()
